7fb0588960e1be71fabb65c611a833064cfc836f
Fatih Gozenc <fatihgozenc@gmail.com>
2019-08-08 19:26:50 +0200

    liebe deine bilder added

---
+++ dev-liebedeinebilder/app/js/app.js
@@ -0,0 +1,19 @@
+// var gallery = new Glide('#gallery', {
+//     type: 'carousel',
+//     autoplay: 2000,
+//     animationDuration: 700,
+//     perView: 4,
+//     breakpoints: {
+//         1200: {
+//             perView: 4
+//         },
+//         960: {
+//             perView: 3
+//         },
+//         600: {
+//             perView: 1
+//         }
+//     }
+// });
+
+// gallery.mount();
