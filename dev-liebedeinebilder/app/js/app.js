da82fd07c58f5fd448df5e972155b9868fa36646
Fatih Gozenc <fatihgozenc@gmail.com>
2019-09-05 19:02:41 +0200

    liebedeinebilder animation entwicklung

--- dev-liebedeinebilder/app/js/app.js
+++ dev-liebedeinebilder/app/js/app.js
@@ -10,7 +10,42 @@ const filterOptions     =   ['option-menschen', 'option-mode', 'option-food', 'o
 const imgTypes          =   ['type-menschen', 'type-mode', 'type-food', 'type-kunst', 'type-produkte', 'type-3d']
                             .map(type => Array.prototype.slice.call(document.getElementsByClassName(type)));  
 
-const allImages         =   Array.prototype.slice.call(document.getElementsByClassName("grid-img-wrapper"));                                              
+const allImages         =   Array.prototype.slice.call(document.getElementsByClassName("grid-img-wrapper"));          
+
+
+const rows2 = document.querySelectorAll(".grid-category");
+
+var firstRow = rows[0];
+// var infiniteLeft = document.querySelector('.infiniteLeft');
+var theWidth = firstRow.getBoundingClientRect().width;
+
+// // function infiniteLeft(element, timing) {
+// //     // var initWidth = element.getBoundingClientRect().width;
+// //     element.style.transform = 'translateX(' + (-theWidth) + 'px)';
+// //     element.style.transitionDuration = timing + 's';
+// //     setTimeout(() => {
+// //         element.style.transitionDuration = 0 + 's';
+// //         element.style.transform = 'translateX(' + 0 + 'px)';
+// //     }, (timing * 1000));
+// // }
+
+function infiniteLeft() {
+    // var initWidth = element.getBoundingClientRect().width;
+    firstRow.style.transform = 'translateX(' + (-theWidth) + 'px)';
+    firstRow.style.transitionDuration = 5 + 's';
+    setTimeout(() => {
+        firstRow.style.transitionDuration = 0 + 's';
+        firstRow.style.transform = 'translateX(' + 0 + 'px)';
+        setTimeout(() => {
+            infiniteLeft();
+        }, 10);
+    }, (5 * 1000));
+};
+
+setTimeout(() => {
+    infiniteLeft(); 
+}, 1);
+
 
 //// DUPLICATING ELEMENTS FOR LOOP ILLUSION (AFTER LOOP INIT) - START
 
@@ -40,22 +75,39 @@ rows.map(category => duplicateChildNodes(category));
 var lightbox = document.querySelector('.grid-lightbox');
 var lightboxText = document.querySelector('.grid-lightbox-text');
 var lightboxGallery = document.querySelector('.grid-lightbox-gallery');
-var lightBoxImgList = Array.prototype.slice.call(document.getElementsByClassName('.grid-lightbox-item'));
+var lightBoxImgList = Array.from(document.querySelectorAll('.grid-lightbox-item'));
 var lightboxWrapper = document.querySelector('.grid-lightbox-wrapper');
 var lightboxHero = document.querySelector('.grid-lightbox-selected');
+var lightboxShown = document.getElementById('grid-lightbox-shown');
+var lightboxSource = document.getElementById('testImg');
 
 allImages.forEach(element => {
-
     element.addEventListener('click', function(){
         lightbox.classList.add('showLightbox');
         setTimeout(() => {
             lightboxWrapper.classList.add('revealPanel');
+            lightboxGallery.classList.add('galleryAppear');
         }, 600);
         lightboxHero.src = element.getElementsByClassName('grid-img')[0].src;
         var imgGroupName = element.classList[1];
         hideAllButSelected(imgGroupName);
     });
 });
+
+function clickToShow(input, targetImg, animClass, time) {
+    input.addEventListener('click', function () {
+        
+        targetImg.classList.remove(animClass);
+        setTimeout(() => {
+            targetImg.src = input.title;
+            targetImg.classList.add(animClass);
+        }, time);
+    })
+};
+
+// function animateText()
+
+lightBoxImgList.map(img => clickToShow(img, lightboxShown, "showImg", 300));
 
 function hideAllButSelected(c) {
     var x, i;
@@ -67,7 +119,18 @@ function hideAllButSelected(c) {
         if (x[i].className.indexOf(c) > -1) addingClass(x[i], "d-inline-block");
     }
 }
+// var sourceList = Array.prototype.slice.call(document.querySelectorAll('.' + source));
+// var shown = document.getElementById(target);
 
+// sourceList.map(source => function () {
+//     source.addEventListener('click', function () {
+//         console.log(source);
+//     })
+// })
+
+
+
+// clickToShow("grid-lightbox-item", "grid-lightbox-shown");
 // filterSelection('type-menschen')" 
 // filterSelection('type-mode')" clas
 // filterSelection('type-food')" clas
@@ -100,7 +163,6 @@ function hideAllButSelected(c) {
 function getNonCloned(imgtype) {
     var imgArray = Array.prototype.slice.call(document.querySelectorAll('[data-lightbox="' + imgtype + '"]'));
     imgArray = imgArray.slice(0, (imgArray.length / 2));
-    log(imgArray);
     return imgArray;
 }
 
@@ -179,39 +241,62 @@ let rowEndCoords = rowWidths.map( rowWidth => {
     return -Math.abs(rowWidth);
 });
 
+function usleep(microseconds) {
+    // Delay for a given number of micro seconds    
+    //   
+    // version: 902.122  
+    // discuss at: http://phpjs.org/functions/usleep  
+    // // +   original by: Brett Zamir  
+    // %        note 1: For study purposes. Current implementation could lock up the user's browser.  
+    // %        note 1: Consider using setTimeout() instead.  
+    // %        note 2: Note that this function's argument, contrary to the PHP name, does not  
+    // %        note 2: start being significant until 1,000 microseconds (1 millisecond)  
+    // *     example 1: usleep(2000000); // delays for 2 seconds  
+    // *     returns 1: true  
+    var start = new Date().getTime();
+    while (new Date() < (start + microseconds / 1000));
+    return true;
+} 
+
 let currentPos = [0, 0, 0, 0];
+let request;
 
-function loopGallery() {
-    currentPos[0] -= .4;
-    currentPos[1] -= .7;
-    currentPos[2] -= 1;
-    currentPos[3] -= 1.5;
+// const loopGallery = () => {
 
-    rows[0].style.transform = "translate3d(" + currentPos[0] + "px , 0, 0)";
-    rows[1].style.transform = "translate3d(" + currentPos[1] + "px , 0, 0)";
-    rows[2].style.transform = "translate3d(" + currentPos[2] + "px , 0, 0)";
-    rows[3].style.transform = "translate3d(" + currentPos[3] + "px , 0, 0)";
+//     // usleep(10000);
 
-    if (currentPos[0] <= rowEndCoords[0]) {
-        currentPos[0] = .4;
-    };
+//     currentPos[0] -= .4;
+//     currentPos[1] -= .7;
+//     currentPos[2] -= 1;
+//     currentPos[3] -= 1.5;
+
+//     rows[0].style.transform = "translate3d(" + currentPos[0] + "px , 0, 0)";
+//     rows[1].style.transform = "translate3d(" + currentPos[1] + "px , 0, 0)";
+//     rows[2].style.transform = "translate3d(" + currentPos[2] + "px , 0, 0)";
+//     rows[3].style.transform = "translate3d(" + currentPos[3] + "px , 0, 0)";
+
+//     if (currentPos[0] <= rowEndCoords[0]) {
+//         currentPos[0] = .4;
+//     };
     
-    if (currentPos[1] <= rowEndCoords[1]) {
-        currentPos[1] = .7;
-    };
+//     if (currentPos[1] <= rowEndCoords[1]) {
+//         currentPos[1] = .7;
+//     };
     
-    if (currentPos[2] <= rowEndCoords[2]) {
-        currentPos[2] = 1;
-    };
+//     if (currentPos[2] <= rowEndCoords[2]) {
+//         currentPos[2] = 1;
+//     };
     
-    if (currentPos[3] <= rowEndCoords[3]) {
-        currentPos[3] = 1.5;
-    };
+//     if (currentPos[3] <= rowEndCoords[3]) {
+//         currentPos[3] = 1.5;
+//     };
 
-    requestAnimationFrame(loopGallery);
-};
+//     request = requestAnimationFrame(loopGallery);
+// };
 
-loopGallery();
+// requestAnimationFrame(loopGallery);
+
+// loopGallery();
 
 // lightbox.option({
 //     'alwaysShowNavOnTouchDevices': false,
