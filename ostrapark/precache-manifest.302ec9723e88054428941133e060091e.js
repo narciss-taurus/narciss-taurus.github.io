80ea68ea9afef378e74cfd5adb625079677164f5
Fatih Gozenc <admin@narciss-taurus.de>
2019-10-25 14:06:08 +0200

    ostra added

---
+++ ostrapark/precache-manifest.302ec9723e88054428941133e060091e.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "026fef0d781db1de379c6861593cea04",
+    "url": "/ostrapark/index.html"
+  },
+  {
+    "revision": "360d9e3086807aecfd6b",
+    "url": "/ostrapark/static/css/2.1fd2b905.chunk.css"
+  },
+  {
+    "revision": "b4ba5e2a3bf2b3e0e0c5",
+    "url": "/ostrapark/static/css/main.724d9324.chunk.css"
+  },
+  {
+    "revision": "360d9e3086807aecfd6b",
+    "url": "/ostrapark/static/js/2.1da970ba.chunk.js"
+  },
+  {
+    "revision": "b4ba5e2a3bf2b3e0e0c5",
+    "url": "/ostrapark/static/js/main.324388fd.chunk.js"
+  },
+  {
+    "revision": "21dfbc6da134eb8241b6",
+    "url": "/ostrapark/static/js/runtime-main.8ef0b7e4.js"
+  },
+  {
+    "revision": "1efbd38aa76ddae2580fedf378276333",
+    "url": "/ostrapark/static/media/lato-v16-latin-700.1efbd38a.woff2"
+  },
+  {
+    "revision": "596223f7bff9598c852e949e9d2b1aa0",
+    "url": "/ostrapark/static/media/lato-v16-latin-700.596223f7.svg"
+  },
+  {
+    "revision": "874b8e7bc7e8d1507b50f56bc6c9b536",
+    "url": "/ostrapark/static/media/lato-v16-latin-700.874b8e7b.woff"
+  },
+  {
+    "revision": "f9203469f750b4284c62aa49f9496cb0",
+    "url": "/ostrapark/static/media/lato-v16-latin-700.f9203469.eot"
+  },
+  {
+    "revision": "fe7d53e4cac8f5b946a9e25f004efc8c",
+    "url": "/ostrapark/static/media/lato-v16-latin-700.fe7d53e4.ttf"
+  },
+  {
+    "revision": "7dfa12661560a957c573a5eff295e819",
+    "url": "/ostrapark/static/media/lato-v16-latin-900.7dfa1266.svg"
+  },
+  {
+    "revision": "947e87c53b5765bfc8982613ccd789e9",
+    "url": "/ostrapark/static/media/lato-v16-latin-900.947e87c5.woff2"
+  },
+  {
+    "revision": "a82ff6ac9208656f9a21a65dfacbcae3",
+    "url": "/ostrapark/static/media/lato-v16-latin-900.a82ff6ac.woff"
+  },
+  {
+    "revision": "d8f0e90eb7c30f7f7780d6136080fb61",
+    "url": "/ostrapark/static/media/lato-v16-latin-900.d8f0e90e.eot"
+  },
+  {
+    "revision": "e7d596b2432d63fedce4e621b130d72f",
+    "url": "/ostrapark/static/media/lato-v16-latin-900.e7d596b2.ttf"
+  },
+  {
+    "revision": "056a373d808ada4f12a2d6538e88823c",
+    "url": "/ostrapark/static/media/lato-v16-latin-regular.056a373d.eot"
+  },
+  {
+    "revision": "9087e4a6aceecc9b2914823044951a3a",
+    "url": "/ostrapark/static/media/lato-v16-latin-regular.9087e4a6.svg"
+  },
+  {
+    "revision": "b4d2c4c39853ee244272c04999b230ba",
+    "url": "/ostrapark/static/media/lato-v16-latin-regular.b4d2c4c3.woff2"
+  },
+  {
+    "revision": "b8ee546acd6cc0c49f42ad3d48ef244f",
+    "url": "/ostrapark/static/media/lato-v16-latin-regular.b8ee546a.woff"
+  },
+  {
+    "revision": "d1d5e30aa8bb081cad8f409f27861672",
+    "url": "/ostrapark/static/media/lato-v16-latin-regular.d1d5e30a.ttf"
+  }
+]);
