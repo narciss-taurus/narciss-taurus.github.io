8c29ade005a112cc1cc95d8e3fca79b21227a9fe
Fatih Gozenc <admin@narciss-taurus.de>
2019-11-07 14:34:54 +0100

    cleaned some repos, added ptfe

---
+++ ptfe/precache-manifest.604e8a8813c8e7e99f477141d5559b16.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ffc5266ca695e88f15816bd58471f4a6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a4d9cd580c54b454e839",
+    "url": "/static/css/main.d002d9f1.chunk.css"
+  },
+  {
+    "revision": "3ad88c6b6a4aab47179b",
+    "url": "/static/js/2.f1014ee0.chunk.js"
+  },
+  {
+    "revision": "a4d9cd580c54b454e839",
+    "url": "/static/js/main.8e148727.chunk.js"
+  },
+  {
+    "revision": "4c71aedf4ced80475805",
+    "url": "/static/js/runtime-main.6e82e578.js"
+  },
+  {
+    "revision": "7284b2714b63ba0d48da5293bee57c20",
+    "url": "/static/media/Alt_PTFE_Nuenchritz_alt.7284b271.jpg"
+  },
+  {
+    "revision": "dc076316ad2abf6f3ed52bc1c1dfcf7c",
+    "url": "/static/media/Neubau_Schild.dc076316.jpg"
+  },
+  {
+    "revision": "8272051004b528ce34183ee695cfdc86",
+    "url": "/static/media/PTFE_Architektur_08.82720510.jpg"
+  }
+]);
